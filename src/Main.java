d821d8d9a6e54e7ebcf49099f161f904c574050b
Yuliya Vorobeva <ys.vorobeva@gmail.com>
2022-06-10 05:36:38 +0700

    Project added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BonusMilesService service = new BonusMilesService();
+        int miles = service.calculate(10_000);
+        System.out.println(miles);
+
+
+    }
+}
